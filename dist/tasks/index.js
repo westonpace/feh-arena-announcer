7518a29332fd6b8bea4e0e22ba608abfc306779f
Weston Pace <weston.pace@gmail.com>
2017-12-17 23:52:01 +0000

    Added basic forever support, removed dist

--- dist/tasks/index.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-function __export(m) {
-    for (var p in m) if (!exports.hasOwnProperty(p)) exports[p] = m[p];
-}
-Object.defineProperty(exports, "__esModule", { value: true });
-__export(require("./poll-matches"));
-__export(require("./report-next-match"));
-//# sourceMappingURL=index.js.map
